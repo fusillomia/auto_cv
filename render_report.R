26142c5de99c52187c01215a5de41a25a690f8ac
fusillomia <mia.fusillo@utexas.edu>
2023-03-22 14:09:16 -0500

    new CV

--- render_report.R
+++ render_report.R
@@ -52,7 +52,7 @@ render_report = function(orcid, email, path){
   }
 }
 
-render_report("0000-0002-7211-0802", "sarah.angulo@utexas.edu", path)
+render_report("0000-0001-8518-8812", "adela.timmons@austin.utexas.edu", path)
 
 
 
